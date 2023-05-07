9fb362fb35fe167d2c1173b1cae2c3853891d5b4
yofreddchs-hub <yofreddchirino@gmail.com>
2023-05-07 10:14:36 -0400

    Incio Multiple 1.3

--- server/src/database.js
+++ server/src/database.js
@@ -2,14 +2,14 @@ require('dotenv').config({path:'./server/variables.env'})
 const mongoose = require('mongoose');
 
 const URI = process.env.DB_URL;
-mongoose.set('useFindAndModify', false);
+// mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
 
 mongoose.connect(URI, {
+    // useFindAndModify: false,
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: true,
+    // useCreateIndex: true
 });
 
 const connection = mongoose.connection;
