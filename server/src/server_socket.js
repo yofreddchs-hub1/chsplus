22d128f52d8b7d3bacb063827a1bcfdfc6ba32ab
yofreddchs-hub <yofreddchirino@gmail.com>
2024-08-26 07:45:28 -0400

    modificacion sistemachs 1.9

--- server/src/server_socket.js
+++ server/src/server_socket.js
@@ -144,9 +144,217 @@ global.io = io.of('/').on('connection', (socket) =>{
 
         global.io.to(socket.id).emit('Sincronizando', {Respuesta:'Ok', ...data, cantidad, datos:dbs, cantidades, pag, fecha, fechaa, fechaf:new Date()})
     })
-});
+    // lo nuevo para trabajo local
+    socket.on('Modificaciones', async(data)=>{
+        const {api, codigo_navegador, tablas, datos, pagina, total, cantidadP} = data; 
+        const DB= await Model(api, tablas[0]);
+        let nuevodatos=[];
+        let npagina = pagina;
+        console.log('......',codigo_navegador, pagina ,datos.length, total)
+        //acauliza segun modificacion de modificaciones
+        if(datos.length!==0){
+            let tablasm=[];
+            for (var j =0; j<datos.length; j++){
+                let valor = datos[j];
+                let clientes=valor.valores.clientes;
+                const pos = clientes.indexOf('servidor');
+                if (pos===-1){
+                    clientes=['servidor',...clientes]
+                    console.log(clientes)
+                }
+                const {tabla} =valor.valores
+                tablasm=[...tablasm, tabla];
+                if(pos===-1 && valor.valores.origen!=='servidor'){
+                    console.log('actualizar valor ...');
+                    let anteriorM= await DB.findOne({'valores._id_registro':valor.valores._id_registro});
+                    if (anteriorM!==null && new Date(anteriorM.updatedAt)>new Date(valor.updatedAt)){
+                        console.log('Actualizar con anterior')
+                        valor= anteriorM;
+                    }
+                    const {_id_registro, newdatos, tipo, origen} =valor.valores;
+                    
+                    const NDB= await Model(api, tabla);
+                    delete newdatos.local
+                    // console.log(newdatos)
+                    if (tipo!=='Eliminar'){
+                        //crea o actualiza registro
+                        delete newdatos.hash_chs
+                        console.log('Crea o actualiza>>>>>>>>>')
+                        await NDB.updateOne({_id:newdatos._id},{...newdatos},{ upsert: true })
+                    }else{
+                        console.log('Eliminar>>>>>>>>>')
+                        await NDB.deleteOne({_id:newdatos._id});
+                    }
+                    //actualiza o crea modificacion
+                    
+                    await DB.updateOne({
+                        _id:valor._id,
+                    },{
+                        valores:{
+                            _id_registro,
+                            tabla,
+                            tipo,
+                            newdatos,
+                            origen,
+                            clientes
+                        }
+                    },{ upsert: true })
 
-Enviar_usuario = async(socket)=>{
+                    const nuevo= await DB.findOne({_id:valor._id});
+                    nuevodatos=[...nuevodatos, nuevo]
+                }else{
+                    // const anterior = await DB.findOne({_id:valor._id})
+                    // if (anterior===null || (anterior!==null && new Date(anterior.updatedAt)>new Date(valor.updatedAt))){
+                        // console.log('>>>')
+                        await DB.updateOne({_id:valor._id},{...valor, valores:{...valor.valores, clientes}},{ upsert: true })
+                    // }
+                }
+                
+                // if(valor.valores.origen==='servidor'){    
+                //     nuevodatos=[...nuevodatos, {...valor, valores:{...valor.valores, clientes}}]
+                // }
+                
+            }
+            global.io.to(socket.id).emit('Actualizar', {tablas: tablasm}) 
+        }
+
+        if (nuevodatos.length!==0){
+            console.log('actualizar modificaciones de:', codigo_navegador)
+            global.io.to(socket.id).emit('Actualizar_modificacion', {codigo_navegador, nuevodatos, pagina})
+            return 
+        }
+        let totals = await DB.estimatedDocumentCount();
+        // if (nuevodatos.length!==0 || total>totals){
+        //     console.log('.....por aqui.....',totals)
+        //     global.io.to(socket.id).emit('Actualizar_modificacion', {codigo_navegador, nuevodatos, pagina})
+        //     return
+        // }else if(datos.length!==0){
+        //     global.io.to(socket.id).emit('Actualizar_modificacion_fin', {codigo_navegador})
+        // }
+
+        //Cacular total de cambios de database
+        let total_cambios = 0;
+        for (var i=1; i<tablas.length;i++){
+            const NDB= await Model(api, tablas[i]);
+            const ntotal = await NDB.estimatedDocumentCount();
+            total_cambios+=ntotal;
+        }
+        // let cambios = await DB.find();
+        if (totals!==total_cambios && totals<total_cambios){//Para actualizar todos los datos
+            console.log('por aqui');
+            npagina=0;
+            await DB.deleteMany(); 
+            for (var i=1; i<tablas.length;i++){
+                const NDB= await Model(api, tablas[i]);
+                const valores = await NDB.find();
+                for (var j=0; j<valores.length; j++){
+                    let nuevo = valores[j];
+                    const _id_registro =`${tablas[i]}-Nuevo-${String(nuevo._id)}`
+                    await DB.updateOne({
+                        "valores._id_registo":_id_registro,
+                    },{
+                        valores:{
+                            _id_registro,
+                            tabla:tablas[i],
+                            tipo:'Nuevo',
+                            newdatos:nuevo,
+                            origen:'servidor',
+                            clientes:['servidor']
+                        }
+                    },{ upsert: true })
+                }
+            }
+            totals = await DB.estimatedDocumentCount();
+            console.log('Listo', totals)
+            // global.io.to(socket.id).emit('Actualizar_local', {Respuesta:'Todos', codigo_navegador})  
+        }
+        const modificaciones = await DB.find().sort({updatedAt:-1});
+        // console.log(total, totals, total_cambios)
+        let cambios = porpagina(modificaciones,npagina,cantidadP,(dato)=>{
+            const pos = dato.valores.clientes.indexOf(codigo_navegador);
+            return pos===-1
+        })
+        if (cambios.resulta.length === 0 && total<totals){
+            cambios = porpagina(modificaciones,npagina,cantidadP,(dato)=>{
+                return true 
+            })
+        }
+        console.log('>>>>> Respuesta', codigo_navegador, cambios.resulta.length, totals)
+        global.io.to(socket.id).emit('Actualizar_local', 
+            { 
+                codigo_navegador,
+                pagina:npagina,
+                cantidadP,
+                datos:cambios,
+                totals
+            }
+        )  
+        if(cambios.resulta.length===0){
+            // setTimeout(async() => {
+                let usuarios= await Enviar_usuario(socket, false);
+                usuarios = usuarios.filter(f=>f.codigo_navegador!==codigo_navegador);
+                let ultimo = await DB.find().sort({updatedAt:-1}).limit(1);
+                ultimo = ultimo.length!==0 ? ultimo[0].valores.clientes : null;
+                if (ultimo){
+                    for (var i=0; i<usuarios.length;i++){
+                        const usuario = usuarios[i];
+                        if (ultimo.indexOf(usuario.codigo_navegador)===-1 && usuario.codigo_navegador){
+                            console.log(codigo_navegador, usuario.codigo_navegador, ultimo);
+                            global.io.emit('Actualizar_local_verifica', 
+                                { 
+                                    codigo_navegador: usuario.codigo_navegador,
+                                    pagina:npagina,
+                                    cantidadP,
+                                    datos:cambios,
+                                    totals
+                                }
+                            )                   
+                        }
+                    }
+                }
+            // }, 1000);
+        }
+
+
+    })
+    socket.on('Actualizando', async(data)=>{
+        const {api, codigo_navegador, tabla, pagina, cantidadP} = data; 
+        console.log('>>>>>....',tabla,pagina,cantidadP)
+        if (tabla===undefined){
+            console.log(chalk.inverse.red(`Tabla actualizando no existe`));
+            return
+        }
+        const DB= await Model(api, tabla);
+        const datos = await DB.find()
+                .limit(cantidadP)
+                .skip(pagina*cantidadP).exec();
+        
+        global.io.to(socket.id).emit('Actualizando_Cliente', {codigo_navegador, tabla, pagina, cantidadP, datos})
+        
+    })
+
+});
+//Para buscar un grupo de datos
+const porpagina = (datos, paginaA, cantidad, condicion)=>{
+    const inicio = paginaA * cantidad;
+    const fin = cantidad;
+    const total = datos.length;
+    let ultimo = inicio;
+    let resulta=[];
+    for (var i=inicio; i<datos.length; i++){
+       const dato = datos[i];
+       ultimo=i;
+       if (condicion(dato)){
+         resulta=[...resulta,dato]
+       }
+       if (resulta.length===fin){
+         break;
+       }
+    }
+    return {resulta, total, ultimo}
+}
+
+Enviar_usuario = async(socket, enviar=true)=>{
     // const User = require(`./models/${socket.handshake.auth.api ? socket.handshake.auth.api+'_':''}User_api`);
     // console.log(`./models/${socket.handshake.auth.api}_User_api`)
     // const usuarios= await User.find();
@@ -159,8 +367,10 @@ Enviar_usuario = async(socket)=>{
         ...socket.handshake.auth,
         });
     }
-    
-    global.io.emit("users", {users, userID:socket.id});
+    if (enviar){
+        global.io.emit("users", {users, userID:socket.id});
+    }
+    return users
     // global.io.emit('Usuario_presentes',usuarios)    
 }
 
