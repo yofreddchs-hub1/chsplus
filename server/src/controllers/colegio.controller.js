d703c19a16080871251654d897683a65b316d99a
yofreddchs-hub <yofreddchirino@gmail.com>
2025-04-04 08:17:06 -0400

    Acomodado detalles 2.2, acomodado notas por docente

--- server/src/controllers/colegio.controller.js
+++ server/src/controllers/colegio.controller.js
@@ -668,12 +668,12 @@ colegioCtrl.Titulos = async(Api,datos)=>{
                 {'valores.periodo':datos.periodo},
                 {'valores.grado':datos.grado},
                 {'valores.seccion':datos.seccion},
-                ... datos.tipo==='docente' 
-                ?   [
-                        {'valores.docente._id':datos.docente._id},
-                        {'valores.asignatura._id':datos.asignatura._id}
-                    ]
-                :   []
+                // ... datos.tipo==='docente' 
+                // ?   [
+                //         {'valores.docente._id':datos.docente._id},
+                //         {'valores.asignatura._id':datos.asignatura._id}
+                //     ]
+                // :   []
             ]
         }
     );
@@ -716,6 +716,7 @@ colegioCtrl.Titulos = async(Api,datos)=>{
             ];
         }
     }else{
+        console.log('por docente >>>>>>>>>>>>')
         evaluaciones.map((val, i)=>{
             if (lapso===null){
                 lapso=val.lapso;
